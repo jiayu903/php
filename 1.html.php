0e60c25d52fa27f35dab10dab794a06eb7f031a3
jiayu903 <82023328+jiayu903@users.noreply.github.com>
2021-11-05 14:10:29 +0800

    Add files via upload

---
+++ 1.html.php
@@ -0,0 +1,17 @@
+<html>
+    <head><title>html+php</title></head>
+    <body>
+        <?php
+            #echo 的功用是用來輸出字串
+            echo "php<br>";
+        ?>
+        <h1>html</h1>
+        <?php
+            echo "php<br>";
+        ?>
+        <h1>html</h1>
+        <?php
+            echo "php<br>";
+        ?>
+    </body>
+</html>
